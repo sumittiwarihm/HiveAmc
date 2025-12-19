a0a3048d594dcddba24a92348c2ed4b8e3b92d90
sumit tiwari <sumit.kumar@hiveminds.in>
2025-12-19 14:48:45 +0530

    jinja template followed

--- Backend/HiveAmc/settings.py
+++ Backend/HiveAmc/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'HiveAmc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.shortcuts import render
 
 @api_view(['GET'])
 def homePageData(request):
+    return render(request, 'home.html')
     
